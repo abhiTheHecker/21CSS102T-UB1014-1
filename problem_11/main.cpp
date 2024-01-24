0c7543a81b05d05c0ed4ae04c070f3ae807064d6
ABIHIMANYU V <142099449+abhiTheHecker@users.noreply.github.com>
2024-01-24 17:41:19 +0530

    Update main.cpp

--- problem_11/main.cpp
+++ problem_11/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,10 +7,23 @@ int main()
 {
     string months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
+    int num_days[12] = {31, 27, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    string user_input;
+    cout << "Enter the month name: ";
+    cin >> user_input;
+
+    int index;
+
     for (int i = 0; i < 12; i++)
     {
-        cout << months[i] << "\n";
+        if (months[i] == user_input)
+        {
+            index = i;
+        }
     }
+
+    cout << "The number of days is " << num_days[index];
 
     return 0;
 }
